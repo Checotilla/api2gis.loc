b77bc77710411d16343f03821b9997a350757293
Valentin Kosarev <astitstudio@gmail.com>
2013-04-05 12:25:57 +0400

    стартовали

---
+++ www/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>api2gis</title>
+
+        <style type="text/css">
+            body{
+                background-color: #ffcc99;
+            }
+            td{
+                padding:30px;
+            }
+            
+            .header{
+                background-color: #ffcccc;
+            }
+            .main{
+                background-color: #ffffff;
+                height: 80%;
+                vertical-align: top;
+            }
+            .footer{
+                background-color: #ffcccc;
+            }
+
+        </style>
+
+    </head>
+    <body>
+
+        <table border="0" width ="70%" align="center" height="800px">
+<!-- HEADER -->
+            <tr class="header">
+                <td>
+                    <h1>api2gis - вытаскиваем справочники</h1>
+
+                </td>
+            </tr>
+
+<!-- MAIN -->
+            <tr class="main">
+                <td>
+
+				
+				
+				
+
+
+                </td>
+            </tr>
+<!-- FOOTER -->
+            <tr class="footer">
+                <td>
+                    <a href="http://imarket30.loc">iMarket 30</a>
+                    |
+                    <a href="http://ymp.imarket30.loc">Yandex-market Parser</a>
+                </td>
+            </tr>
+        </table>
+
+    </h3>
+</body>
+</html>
